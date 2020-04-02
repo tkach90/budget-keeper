364578fbec38e1b111ec679d1afaa0e6ca78d10d
thach90 <tkach90@yahoo.com.ua>
2020-04-02 09:54:36 +0300

    create base structure, create global, budget and UI controllers, create logic to get input data, add Event Listeners for Enter btn and click event

---
+++ app.js
@@ -0,0 +1,54 @@
+// Budget controller
+const budgetController = (function () {
+
+})();
+
+// UI Controller
+const UIController = (function () {
+    const DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+    }
+
+})();
+
+// Global App controller
+const controller = (function (budgetCtrl, UICtrl) {
+    const DOM = UICtrl.getDOMstrings();
+
+    const ctrlAddItem = function() {
+        // 1. Get the filed input data
+        const input = UICtrl.getInput();
+        console.log(input);
+        // 2. Add the item to the budget controller
+        // 3. Add the item to the UI
+        // 4. Calculate the budget
+        // 5. Display the budget on the UI
+
+    };
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (event) {
+        if ( event.key === 13 ) {
+            ctrlAddItem();
+        }
+    });
+
+})(budgetController, UIController);
