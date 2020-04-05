c3b95ce7f9a5166de0975d8136832542fa9be2ce
thach90 <tkach90@yahoo.com.ua>
2020-04-05 13:49:31 +0300

    fix bug with possibility to add empty input fields, add parseFloat function to value input field

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ const UIController = (function () {
             return {
                 type: document.querySelector(DOMstrings.inputType).value,
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             }
         },
 
@@ -130,27 +130,32 @@ const controller = (function (budgetCtrl, UICtrl) {
         });
     };
 
+    const updateBudget = function () {
+        // 1. Calculate the budget
 
+        // 2. Return the budget
+
+        // 3. Display the budget on the UI
+    };
 
     const ctrlAddItem = function() {
         let input, newItem;
         // 1. Get the filed input data
         input = UICtrl.getInput();
 
-        // 2. Add the item to the budget controller
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+            // 2. Add the item to the budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-        // 3. Add the item to the UI
-        UICtrl.addListItem(newItem, input.type);
+            // 3. Add the item to the UI
+            UICtrl.addListItem(newItem, input.type);
 
-        // 4. Clear the fields
-        UICtrl.clearFields();
+            // 4. Clear the fields
+            UICtrl.clearFields();
 
-        // 5. Calculate the budget
-
-
-        // 6. Display the budget on the UI
-
+            // 5. Calculate and update budget
+            updateBudget();
+        }
     };
 
     return {
