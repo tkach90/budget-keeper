375e7dd09749e7c7dc2705efcfb749c0162a5c8d
thach90 <tkach90@yahoo.com.ua>
2020-04-05 10:16:02 +0300

    add clearFields method to UI controller to clear field description and value after adding new item

--- app.js
+++ app.js
@@ -94,6 +94,21 @@ const UIController = (function () {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
+        clearFields: function() {
+            let fields, fieldsArr;
+            fields = document.querySelectorAll(
+                DOMstrings.inputDescription + ', ' + DOMstrings.inputValue
+            );
+
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function (current, index, array) {
+                current.value = "";
+            });
+
+            fieldsArr[0].focus();
+        },
+
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -128,8 +143,13 @@ const controller = (function (budgetCtrl, UICtrl) {
         // 3. Add the item to the UI
         UICtrl.addListItem(newItem, input.type);
 
-        // 4. Calculate the budget
-        // 5. Display the budget on the UI
+        // 4. Clear the fields
+        UICtrl.clearFields();
+
+        // 5. Calculate the budget
+
+
+        // 6. Display the budget on the UI
 
     };
 
